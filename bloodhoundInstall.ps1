0b8ad4cc7d7d617d6a4008be1b62cb8ecf8be186
kgcencosec <67707744+kgcencosec@users.noreply.github.com>
2021-01-15 10:38:15 -0800

    Adding more feedback for user.

--- bloodhoundInstall.ps1
+++ bloodhoundInstall.ps1
@@ -1,7 +1,7 @@
 <#
 This is a script to download and install all the necessary components to run Bloodhound and Sharphound. 
 It will also install Firefox, if you don't want or need that, comment it out or remove it.
-WARNING: Both Bloodhound.exe and Sharpound.exe will be detected and blocked by most AVs so prep accordingly.
+WARNING: Both BloodHound.exe and SharpHound.exe will be detected and blocked by most AVs so prep accordingly.
 
 By Kyle Gustafson
 #>
@@ -32,12 +32,16 @@ cd $bloodHoundDir
 
 # Unattended Install of Firefox
 
+Write-Host "Installing Firefox..."
+
 wget $SourceURL -OutFile $firefoxInstaller -ErrorAction SilentlyContinue
 Start-Process -FilePath $firefoxInstaller -Args "/q" -ErrorAction SilentlyContinue
 
 Start-Sleep 30
 
 Remove-Item $firefoxInstaller
+
+Write-Host "Firefox Installed. Installing Java..."
 
 #Install OpenJDK 11
 cd $bloodHoundDir
@@ -47,8 +51,12 @@ cd $bloodHoundDir
 wget $openJDKURL -OutFile openjdk.zip -ErrorAction SilentlyContinue
 Expand-Archive .\openjdk.zip -DestinationPath "C:\Program Files\Java" -ErrorAction SilentlyContinue
 
+Write-Host "Setting up Java variables. If the script pauses and is not progressing after a bit, hit the space bar." 
+
 setx -m JAVA_HOME "C:\Program Files\Java\jdk-11"
 setx -m PATH "%PATH%;%JAVA_HOME%\bin";
+
+Write-Host "Java Installed. Installing neo4j..."
 
 #Install Neo4j
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
@@ -59,6 +67,8 @@ Expand-Archive .\neo4j.zip -DestinationPath "C:\Bloodhound\neo4j"
 
 [Environment]::SetEnvironmentVariable("JAVA_HOME", "C:\Program Files\Java\jdk-11")
 
+Write-Host "neo4j Installed. If the script pauses and is no progressing after a bit, hit the space bar again."
+
 cd $neo4jbin
 
 cmd.exe /c ".\neo4j.bat install-service"
@@ -66,6 +76,8 @@ cmd.exe /c ".\neo4j.bat install-service"
 cmd.exe /c "net start neo4j"
 
 #Install Bloodhound
+
+Write-Host "Installing BloodHound and SharpHound..."
 
 cd $bloodHoundDir
 
@@ -99,7 +111,7 @@ cd $bloodHoundDir
 wget $sharpHoundUrl -OutFile SharpHound.exe -ErrorAction SilentlyContinue
 set-shortcut "C:\Users\Public\Desktop\SharpHound.lnk" "C:\Bloodhound\sharphound.exe" -ErrorAction SilentlyContinue
 
-Write-Host "Open a web browser and navigate to http://localhost:7474/. You should see the neo4j web console.
+Write-Host "Install complete. Open a web browser and navigate to http://localhost:7474/. You should see the neo4j web console.
 Authenticate to neo4j in the web console with username neo4j, password neo4j. You’ll be prompted to change this password. 
 
 To run SharpHound with Defaults, double click on the link. To run it with arguments, open a command prompt and cd to the C:\Bloodhound directory."
